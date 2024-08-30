a97199a2f943e6e4fc159c856351ec6340c64d56
Aadarsh-Mane <onlyaddy68@gmail.com>
2024-08-31 01:58:06 +0530

    initial commit

---
+++ data/index.ts
@@ -0,0 +1,202 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Services", link: "#services" }, // New section for services
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+export const gridItems = [
+  {
+    id: 1,
+    title: "Client-Centered Approach",
+    description:
+      "At 20sdevelopers, we prioritize client collaboration, fostering open communication to deliver tailored IT solutions.",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Global Reach",
+    description:
+      "We are flexible with time zone communications, making it easy to collaborate with clients worldwide.",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Our Tech Stack",
+    description:
+      "20sdevelopers continuously improves its tech stack to stay ahead in the industry.",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Passion for Development",
+    description:
+      "Our team at 20sdevelopers is driven by a passion for tech and development.",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+  {
+    id: 5,
+    title: "Innovative Projects",
+    description:
+      "Currently building cutting-edge solutions tailored to our clients' needs.",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Let's Work Together",
+    description: "Contact 20sdevelopers to start your project today.",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Business Website Redesign",
+    des: "Redesigned a corporate website to enhance user experience and boost online visibility.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/js.svg", "/fm.svg"],
+    link: "/ui.business.com",
+  },
+  {
+    id: 2,
+    title: "Marketing Campaign Management Tool",
+    des: "Developed a robust tool for managing and analyzing marketing campaigns effectively.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.marketingtool.com",
+  },
+  {
+    id: 3,
+    title: "E-Commerce Platform",
+    des: "Built a scalable e-commerce platform with integrated payment and inventory management.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.ecommerce.com",
+  },
+  {
+    id: 4,
+    title: "Custom CRM Solution",
+    des: "Created a custom CRM system to streamline business processes and improve customer relationships.",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/gsap.svg"],
+    link: "/ui.crm.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "20sdevelopers transformed our website into a dynamic, user-friendly platform. Their dedication to our project was unmatched, and they delivered on time and beyond expectations.",
+    name: "Sarah Williams",
+    title: "CEO of Tech Innovators",
+  },
+  {
+    quote:
+      "The team at 20sdevelopers provided excellent service and communication. Their ability to adapt to our needs and their technical expertise made our project a success.",
+    name: "David Brown",
+    title: "Marketing Manager at BrandCorp",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev - JSM Tech",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
